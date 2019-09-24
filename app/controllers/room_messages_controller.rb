50c43e125cbdc27de4d9b024525dac038f51bf93
aditya suhag <adityasuhag@gmail.com>
2019-09-24 12:49:43 +0530

    added Room index and creation

--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -6,7 +6,9 @@ def create
 											 room: @room,
 											 message: params.dig(:room_message, :message)
 end
+
 protected
+
 def load_entities
     @room = Room.find params.dig(:room_message, :room_id)
 end
